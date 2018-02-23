f93f78c6b0f8a1f1309cd94621e748f1bcc013ee
陈晟 <251382101@qq.com>
2018-02-23 14:50:38 +0800

    Initial commit

---
+++ src/auto/AutoProperties.java
@@ -0,0 +1,10 @@
+package auto;
+
+public class AutoProperties {
+
+	public static final String jdbcdriver = "com.mysql.jdbc.Driver";
+	public static final String jdbcurl = "jdbc:mysql://localhost:3306/quartz";
+	public static final String username = "root";
+	public static final String password = "123456";
+
+}
